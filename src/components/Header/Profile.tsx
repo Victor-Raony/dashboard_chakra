e8cca6e599fcfd7f9fa9f82da7f98f8b58f2c3d4
Victor Raony Moraes Lima <victorraony@MacBook-Pro-de-Victor.local>
2024-04-19 10:41:32 -0300

    feat: Implementing Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import {Flex, Box, Text, Avatar} from "@chakra-ui/react"
 
-export default function Profile(){
+interface ProfileProps{
+  showProfileData?: boolean;
+}
+
+export default function Profile({showProfileData = true }: ProfileProps){
   return(
     <Flex>
-    <Box mr="4" textAlign="right">
-      <Text>Victor Raony</Text>
-      <Text color="gray.300" fontSize="small">
-        victor_raony12@hotmail.com
-      </Text>
-    </Box>
+    { showProfileData && (
+      <Box mr="4" textAlign="right">
+        <Text>Victor Raony</Text>
+        <Text color="gray.300" fontSize="small">
+          victor_raony12@hotmail.com
+        </Text>
+      </Box>
+    )}
 
     <Avatar size="md" name="Victor Raony" src="https://github.com/Victor-Raony.png"/>
   </Flex>
